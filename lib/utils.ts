b2539b920df6684e22bd89682873230280edf729
David Hurley <dhurls99@gmail.com>
2024-12-17 15:10:15 -0500

    add logos

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export function cultLogo(address: string) {
   const logos: Record<string, string> = {
-    '0x6b5e56ba879bd0ca182b723357971e039603f6c4': 'higher.jpg',
+    '0x27a040f96bEB380029Ffa15b3E0F0Feec2893Fb6': 'higher.jpg',
+    '0x762a39c8741918216e5c79B31Bb7D30d6d912e9b': 'gdupi.jpg',
+    '0x2EdE573A5F8269FCb8acf648AdfcFb1dE1Cfc879': 'anon.jpg',
+    '0xc7a86D22DFe2B63B5F6ef88087598D52D122d41A': 'comicsans.jpg',
   };
   return logos[address.toLowerCase()];
 }
