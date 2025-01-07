cb1f85796dbd581683165a9f08528bd71943a6b6
David Hurley <dhurls99@gmail.com>
2025-01-07 15:43:58 -0500

    change manifest file

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,18 +3,18 @@ export async function GET() {
     accountAssociation: {
       header:
         'eyJmaWQiOjc5ODgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg1MUQ0NDZFOTNhMTcxZGQxMkY4NGI3NWE4RDFCNjgyNTVGN2MxRjgyIn0',
-      payload: 'eyJkb21haW4iOiJlbmpveXIudmVyY2VsLmFwcCJ9',
+      payload: 'eyJkb21haW4iOiJlbmpveXIyLnZlcmNlbC5hcHAifQ',
       signature:
-        'MHg0MmYyMDE4ZTc5ZjQ3ZTkwNmM1NWNkOGU5NmMzODBmYjU1OGNjZmRjZjc2MjhlNWY0ZTg0NDQ0NDJmODNkODI0MDMxODRlNDc2OGE1NzI2NTA4MjBmMTg5MmZhNWI0ZTQzMTg2ZjA4YTcyZjQwMTRiY2Q3MTZiYTU4ZGI0ZTFjODFj',
+        'MHg2ZmQ0OTZiMTA3ODI5ZjcyYTQzZDQ0MDI4NGM3ZTdjNzFhNTNjODRkMjQ0ZTg1ZDE0MWFhY2IwNjhjNDk1OTRhNWJiZjJiYTIyNjcwYjA4MGI2ZmFiMGE2MDJhODZkNTNhMmFmZjUzOTVkMjA0ZjIwODQyMzBkMDQxMWQ2ZDIxMzFj',
     },
     frame: {
       version: '1',
-      name: 'Enjoyr (Zora Network)',
-      iconUrl: 'https://enjoyr2.vercel.app/logo.png',
+      name: 'Example Frame',
+      iconUrl: 'https://enjoyr2.vercel.app/icon.png',
       homeUrl: 'https://enjoyr2.vercel.app',
-      imageUrl: 'https://enjoyr2.vercel.app/logo.png',
-      buttonTitle: 'Launch Frame',
-      splashImageUrl: 'https://enjoyr2.vercel.app/logo.png',
+      imageUrl: 'https://enjoyr2.vercel.app/image.png',
+      buttonTitle: 'Check this out',
+      splashImageUrl: 'https://enjoyr2.vercel.app/splash.png',
       splashBackgroundColor: '#eeccff',
       webhookUrl: 'https://enjoyr2.vercel.app/api/webhook',
     },
